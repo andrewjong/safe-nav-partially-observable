d725de709dff2038d132c835b523c137f27e7348
Andrew Jong <ajong@andrew.cmu.edu>
2025-04-20 17:53:28 -0400

    add comment

--- run/racecar_warm_hj.py
+++ run/racecar_warm_hj.py
@@ -16,7 +16,6 @@ def main():
     env = posggym.make('DrivingContinuous-v0', world="30x30ScatteredObstacleField", num_agents=1, n_sensors=N_SENSORS, render_mode="human")
 
 
-    # failure_map = FailureMapBuilder(size_x, size_y)
 
     solver = WarmStartSolver(
         config=WarmStartSolverConfig(
@@ -39,6 +38,10 @@ def main():
     observations, infos = env.reset()
 
 
+    # creates a failure map with the given width, height and resolution.
+    # unobserved cells are initialized as fail set.
+
+    failure_map = FailureMapBuilder(MAP_WIDTH, MAP_HEIGHT, MAP_RESOLUTION)
     for _ in range(300):
         actions = {i: env.action_spaces[i].sample() for i in env.agents}
         observations, rewards, terminations, truncations, all_done, infos = env.step(actions)
@@ -56,8 +59,9 @@ def main():
 
         print(f"{vehicle_x=}, {vehicle_y=}, {vehicle_angle=}, {vehicle_x_velocity=}, {vehicle_y_velocity=}")
 
-        # update the fail set from the lidar observations. cells that are free are marked as safe
-        # failure_map.update
+        # update the fail set from the lidar observations. cells that are free are marked as safe.
+        # assumes the lidar observations are equally spaced from 0 to 2*pi
+        failure_map.update_from_lidar(lidar_distances, vehicle_x, vehicle_y, vehicle_angle)
 
 
 
