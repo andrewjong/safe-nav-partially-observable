a4907335f55d35c9de9b8805f8159632ab91aebb
Andrew Jong <ajong@andrew.cmu.edu>
2025-05-01 17:41:06 -0400

    Remove code

--- reachability/warm_start_solver.py
+++ reachability/warm_start_solver.py
@@ -85,53 +85,6 @@ class Dubins3D(dynamics.ControlAndDisturbanceAffineDynamics):
         )
 
 
-# class Dubins3DVelocity(dynamics.ControlAndDisturbanceAffineDynamics):
-#     def __init__(
-#         self,
-#         max_acceleration=1.0,
-#         max_curvature=1.0,
-#         max_position_disturbance=0.25,
-#         control_mode="min",
-#         disturbance_mode="max",
-#         control_space=None,
-#         disturbance_space=None,
-#     ):
-#         if control_space is None:
-#             control_space = hj.sets.Box(
-#                 jnp.array([-max_acceleration, -max_curvature]),
-#                 jnp.array([max_acceleration, max_curvature]),
-#             )
-#         if disturbance_space is None:
-#             disturbance_space = hj.sets.Ball(jnp.zeros(2), max_position_disturbance)
-#         super().__init__(
-#             control_mode, disturbance_mode, control_space, disturbance_space
-#         )
-
-#     def open_loop_dynamics(self, state, time):
-#         _, _, v, q = state
-#         return jnp.array([v * jnp.cos(q), v * jnp.sin(q), 0.0, 0.0])
-
-#     def control_jacobian(self, state, time):
-#         v = state[2]
-#         return jnp.array(
-#             [
-#                 [0.0, 0.0],
-#                 [0.0, 0.0],
-#                 [1.0, 0.0],
-#                 [0.0, v],
-#             ]
-#         )
-
-#     def disturbance_jacobian(self, state, time):
-#         return jnp.array(
-#             [
-#                 [1.0, 0.0],
-#                 [0.0, 1.0],
-#                 [0.0, 0.0],
-#                 [0.0, 0.0],
-#             ]
-#         )
-
 class Dubins3DVelocity(dynamics.ControlAndDisturbanceAffineDynamics):
     def __init__(self,
                  max_turn_rate=1.,
